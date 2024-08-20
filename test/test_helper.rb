a38457a2f5285afa5bd2191ebab191db16d47ab4
Carlos A <cayala.w@gmail.com>
2024-08-19 19:07:55 -0500

    Complete advanced testing setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 require 'simplecov'
 SimpleCov.start 'rails'
 
